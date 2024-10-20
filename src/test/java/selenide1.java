eb46fefe6b6ce0ff0e711e76957d894bb184b6f2
Vladimir <letuchikh@yandex.ru>
2024-10-20 21:28:33 +0300

    completed github

---
+++ src/test/java/selenide1.java
@@ -0,0 +1,55 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class selenide1 {
+    @BeforeAll
+    static void setup() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link button").click();
+        $("#wiki-pages-box").shouldBe(visible).$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$$("#wiki-pages-box summary a").findBy(text("SoftAssertions")).shouldBe(visible).click();
